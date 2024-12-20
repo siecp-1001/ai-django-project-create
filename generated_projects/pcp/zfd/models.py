7a58593f41b1af62a49d4159fffc6199a45ac8a3
Ahmed Ayman <108583424+siecp-1001@users.noreply.github.com>
2024-12-20 14:42:42 +0200

    up

--- generated_projects/pcp/zfd/models.py
+++ generated_projects/pcp/zfd/models.py
@@ -1,14 +1,7 @@
 from django.db import models
 
-class get_books(models.Model):
-    path = models.books
-
-class add_book(models.Model):
-    path = models.books/add
-
-class get_authors(models.Model):
-    path = models.authors
-
-class add_author(models.Model):
-    path = models.authors/add
+class list_products(models.Model):
+    path = models.products
+    method = models.GET
+    description = models.List all products
 
